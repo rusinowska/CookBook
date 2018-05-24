e7be08d0213bf31410d43e4a92becfad4f990502
Rusinowska Zofia <14_rusinowska@cis.wzks.uj.edu.pl>
2018-05-24 23:46:38 +0200

    Initial commit

---
+++ app/src/AppBundle/Controller/RecipesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zrusinowska
+ * Date: 24/05/2018
+ * Time: 23:40
+ */
+
+namespace AppBundle\Controller;
+
+
+class RecipesController
+{
+
+}
---
+++ app/web/cc.php
@@ -0,0 +1,3 @@
+<?php
+var_dump(exec('chmod -R 777 PATH_TO_USER_HOME_DIR/app/var/cache/*'));
+var_dump(exec('chmod -R 777 PATH_TO_USER_HOME_DIR/app/var/logs/*'));
